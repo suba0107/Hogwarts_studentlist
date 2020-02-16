4c133c82e7a1df60fea21d33df8d829213720b5e
Subangi <suba0107@stud.kea.dk>
2020-02-16 20:59:28 +0100

    Data cleaned and stored in a global array

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let students;
-const list = document.querySelector("#list");
-const temp = document.querySelector("template");
+"use strict";
+window.addEventListener("DOMContentLoaded", start);
+
+const HTML = {};
+
 let studentHouse;
 
 const Student = {
@@ -11,42 +13,124 @@ const Student = {
   image: "",
   house: ""
 };
-document.addEventListener("DOMContentLoaded", getJson);
 
+function start() {
+  HTML.list = document.querySelector("#list");
+  HTML.temp = document.querySelector("template");
+  HTML.allStudents = [];
+  HTML.search = " ";
+  HTML.Student = {
+    firstname: "",
+    lastname: "",
+    middlename: "",
+    nickname: "",
+    image: "",
+    house: ""
+  };
+  getJson();
+}
 async function getJson() {
-  let jsonData = await fetch("students1991.json");
-  students = await jsonData.json();
-  showList();
-  themeSelector();
+  let jsonData = await fetch("https://petlatkea.dk/2020/hogwarts/students.json");
+  let jsonObjects = await jsonData.json();
+  // showList();
+  // themeSelector();
+  prepareObjects(jsonObjects);
 }
 
-function showList() {
-  students.forEach(student => {
-    let clone = temp.cloneNode(true).content;
-    clone.querySelector(".name").textContent = student.fullname;
-    clone.querySelector(".house").textContent = student.house;
-    list.appendChild(clone);
-    list.lastElementChild.addEventListener("click", () => {
-      showSingle(student);
-    });
+function prepareObjects(jsonObjects) {
+  jsonObjects.forEach(jsonObject => {
+    const student = Object.create(HTML.Student);
+    HTML.allStudents.push(student);
+    const fullName = jsonObject.fullname;
+    let house = jsonObject.house;
+    const houseToLowerCase = house.toLowerCase();
+    house = houseToLowerCase.trim();
+    house = house.charAt(0).toUpperCase() + house.slice(1);
+    const fullNameToLowerCase = fullName.toLowerCase();
+    let trimFullName = fullNameToLowerCase.trim();
+    const search = '"';
+    const indexOfFirst = trimFullName.indexOf(search);
+    const hyphenSplit = trimFullName.split("-");
+    let fullNameHyphen;
+    if (trimFullName.includes("-") == true) {
+      fullNameHyphen = hyphenSplit[1].charAt(0).toUpperCase() + hyphenSplit[1].slice(1);
+      hyphenSplit[1] = "-" + fullNameHyphen;
+    }
+    const fullNameJoined = hyphenSplit.join();
+    trimFullName = fullNameJoined.replace(",", "");
+    const indexOfLast = trimFullName.lastIndexOf(search);
+    const nickNameSubstring = trimFullName.substring(indexOfFirst - 1, indexOfLast + 1);
+    const nickNameTrimmed = nickNameSubstring.replace(/"/g, "").trim();
+    const fullNameWithoutNickName = trimFullName.replace(nickNameSubstring, "");
+    const fullNameSplit = fullNameWithoutNickName.split(" ");
+    const fullNameArray = [];
+    for (let i = 0; i < fullNameSplit.length; i++) {
+      fullNameSplit[i].charAt(0).toUpperCase() + fullNameSplit[i].slice(1);
+      fullNameArray.push(fullNameSplit[i].charAt(0).toUpperCase() + fullNameSplit[i].slice(1));
+    }
+    const firstName = fullNameArray[0];
+    let lastName;
+    if (fullNameArray.length > 1) {
+      lastName = fullNameArray[fullNameArray.length - 1];
+    } else {
+      lastName = "";
+    }
+    fullNameArray.pop();
+    fullNameArray.shift();
+    const middleNameJoin = fullNameArray.join();
+    const middleName = middleNameJoin.replace(",", " ");
+    const nickName = nickNameTrimmed.charAt(0).toUpperCase() + nickNameTrimmed.slice(1);
+    student.firstname = firstName;
+    student.lastname = lastName;
+    student.middlename = middleName;
+    student.nickname = nickName;
+    student.house = house;
+    if (student.middlename === "") {
+      delete student.middlename;
+    }
+    if (student.nickname === "") {
+      delete student.nickname;
+    }
+    if (student.lastname === "") {
+      delete student.lastName;
+    }
+  });
+  displayList();
+}
+
+function displayList() {
+  document.querySelector("#list").innerHTML = "";
+  HTML.allStudents.forEach(displayStudent);
+}
+
+function displayStudent(student) {
+  //create clone
+  const clone = HTML.temp.content.cloneNode(true);
+  //set clone data
+  clone.querySelector(".name").textContent = student.firstname + " " + student.lastname;
+  clone.querySelector(".house").textContent = student.house;
+  //append clone to list
+  HTML.list.appendChild(clone);
+  list.lastElementChild.addEventListener("click", () => {
+    showSingle(student);
+    themeSelector(student);
   });
 }
 
 function showSingle(student) {
   document.querySelector("#popup").style.display = "flex";
   document.querySelector("#popup .close").addEventListener("click", closeSingle);
-  document.querySelector(".name").textContent = student.fullname;
+  document.querySelector(".fullname").textContent = student.firstname + " " + student.nickname + " " + student.middlename + " " + student.lastname;
+
   document.querySelector(".house").textContent = student.house;
-  studentHouse = student.house.toLowerCase();
-  themeSelector();
 }
 
 function closeSingle() {
   document.querySelector("#popup").style.display = "none";
 }
 
-function themeSelector() {
-  document.querySelector(".content").dataset.selected = studentHouse;
-  document.querySelector(".close").dataset.selected = studentHouse;
-  document.querySelector(".crest").src = `crests/${studentHouse}.png`;
+function themeSelector(student) {
+  document.querySelector(".content").dataset.selected = student.house.toLowerCase();
+  document.querySelector(".close").dataset.selected = student.house.toLowerCase();
+  document.querySelector(".crest").src = `crests/${student.house.toLowerCase()}.png`;
 }
